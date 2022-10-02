728f85a5d13bb5025eb1fba5663b4c507b48279b
Zulay Ortegon <zulyortegon15@gmail.com>
2022-10-02 18:18:48 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 addEventListener("DOMContentLoaded", ()=>{
-  // 6. Un avión que viaja 800 Km/hr. Dispara un proyectil auto impulsado, en el momento del disparo, el avión hace un giro de 90 grados y acelera a 20 mtrs/seg2. El proyectil sigue su curso, acelerando a 10 mtrs./seg2. Diseñe un pseudocódigo que escriba cada segundo, la distancia que separa al avión del proyectil, hasta que estén a 10,000 mtrs. o más.}
+  // 7. Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana. Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de sus ventas. El gerente de su compañía desea saber cuanto dinero obtendrá en la semana cada vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando en cuenta su sueldo base y sus comisiones.
 
-  console.log(`I don´t speak taka taka :v `)
+  cont=1
+  let e = prompt("Cantidad de empleados:")
+  let sueldobase = prompt("Sueldo base:")
+  diezxc=(sueldobase*10)/100
+  comisión=diezxc*3
+  total=sueldobase+comisión
+
+  while (cont <= e){
+    cont += 1
+    let v1 = prompt("Valor venta 1:")
+    let v2 = prompt("Valor venta 2:")
+    let v3 = prompt("Valor venta 3:")
+    console.log(`El 10% de su sueldo es:  ${diezxc} \n La comisión por sus ventas es: ${comisión} \n El valor total a pagar es: ${total}`)
+    
+  }
 })
