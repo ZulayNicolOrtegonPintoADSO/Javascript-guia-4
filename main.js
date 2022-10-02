18a3330715045fef391d0c0f290e7cc76a23e1e3
Zulay Ortegon <zulyortegon15@gmail.com>
2022-10-02 18:51:55 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
 addEventListener("DOMContentLoaded", ()=>{
-  //9. Diseñe un algoritmo que determine el resultado de la elección del representante estudiantil de la universidad X, para ello se presentaron tres candidatos A, B, y C. Para ganar la elección se debe obtener como mínimo el 51%. En caso que no haya un ganador se repite la elección en una segunda vuelta. Van a la segunda vuelta los dos candidatos que obtengan la más alta votación. Se anula la elección en caso de producirse un empate doble por el segundo lugar o un empate triple.
+  //10. Se está realizando un estudio del tráfico en una de las calles de su ciudad. Para ello deberá controlar durante un cierto tiempo los vehículos que pasan por dicha calle. Los distintos tipos de vehículo será: Turismo, Autobús, Camión o Motocicleta. Además, si el tipo de vehículo es Turismo debe anotar el número de ocupantes. El programa deberá contar 200 vehículos, e ir guardando cuantos pasan de cada tipo. Finalmente mostrará el porcentaje de cada uno de los tipos.
 
-  let a = Number(prompt("CANTIDAD DE VOTOS DEL CANDIDATO A :"))
-  let b = Number(prompt("CANTIDAD DE VOTOS DEL CANDIDATO B :"))
-  let c = Number(prompt("CANTIDAD DE VOTOS DEL CANDIDATO C :"))
+  i=0
+  t=0
+  a=0
+  c=0
+  m=0
 
-  cv=a+c+b
-  console.log(`Cantidad de votos en total = ${cv}`)
-  pa=a/cv
-  pb=b/cv
-  pc=c/cv
-  console.log(`Porcentaje a = ${pa} \n Porcentaje b = ${pb} \n Porcentaje c = ${pc}`)
+  while (i < 201){
+      console.log("Los distintos tipos de vehículo será: 1)Turismo, 2)Autobús, 3)Camión o 4)Motocicleta.")
+      tipo=int(input())
 
-  if(pa == pb & pa == pc){
-    console.log(`ANULADA LA ELECCIÓN POR TRIPLE EMPATE`)
+      if (tipo==1){
+        console.log("ESCOGIÓ LA OPCIÓN TURISMO")
+        t += 1 
+        i += 1
+        console.log(" La cantidad de vehículos de esta categoria que van hasta el momento son: " , t)
+      }
+      if (tipo==2){
+        console.log("ESCOGIÓ LA OPCIÓN AUTOBÚS")
+        a += 1 
+        i += 1
+        console.log(" La cantidad de vehículos de esta categoria que van hasta el momento son: " , a)
+      }
+      if (tipo==3){
+        console.log("ESCOGIÓ LA OPCIÓN CAMIÓN")
+        c += 1 
+        i += 1
+        console.log(" La cantidad de vehículos de esta categoria que van hasta el momento son: " , c)
+      }
+      if (tipo==4){
+        console.log("ESCOGIÓ LA OPCIÓN MOTOCICLETA")
+        m += 1 
+        i += 1
+        console.log(" La cantidad de vehículos de esta categoria que van hasta el momento son: " , m)
+      }
+      if (tipo < 1 | tipo > 4){
+          console.log("ESA OPCIÓN NO ESTÁ DISPONIBLE")
+      }
   }
-  else{
-    if(pa == pb & pa == pc){
-      console.log(`El candidato A es el ganador`)
-    }
-    if(pb > 0.51){
-      console.log(`El candidato B es el ganador`)  
-    }
-    if(pc > 0.51){
-      console.log(`El candidato C es el ganador`) 
-    }
+  pt=t/i*100
+  pa=a/i*100
+  pc=c/i*100
+  pm=m/i*100
 
-    if(pa == pb){
-      console.log(`ANULADO por empate entre los candidatos A y B`) 
-    }
-    if(pa == pc){
-      console.log(`ANULADO por empate entre los candidatos A y C`) 
-    }
-    if(pb == pc){
-      console.log(`ANULADO por empate entre los candidatos B y C`) 
-    }
-  }
+  console.log("El porcentaje de vehículos de tipo turismo es:  " , pt)
+  console.log("El porcentaje de vehículos de tipo autobús es:  " , pa)
+  console.log("El porcentaje de vehículos de tipo camión es:  " , pc)
+  console.log("El porcentaje de vehículos de tipo motocicleta es:  " , pm)
+
+  console.log("EJECUTADO CON EXITO")
 
 })
