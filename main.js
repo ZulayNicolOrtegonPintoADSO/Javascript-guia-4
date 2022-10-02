0f43cb29eccb477c73c709e0449156e83401c69a
Zulay Ortegon <zulyortegon15@gmail.com>
2022-10-02 17:46:25 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-  // 4. 42 personas toman parte de un baile. Durante la fiesta una dama bailó con 7 caballeros, una segunda dama con 8, una tercera con 9, y así sucesivamente hasta que la última bailó con todos los hombres. Cuantas damas había en el baile?
+  // 3. Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de computación.
 
-  cont=7
-  acum=0
-  while (cont <= 42){
-    cont += 1
-    acum += 1 
+  cont=0
+  cantidad=5 
+  while (cont < cantidad){
+    let n = prompt("Nombre del estudiante:")
+    let a = Number(prompt("Apellido del estudiante:"))
+    let e = Number(prompt("Edad del estudiante:"))
+    let p = Number(prompt("Promedio del estudiante:"))
   }
-  console.log(`Habían ${acum} damas en el baile`)
+  console.log(`FIN PROGRAMA`)
 })
